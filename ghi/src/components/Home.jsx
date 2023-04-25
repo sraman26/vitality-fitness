ae4c117a5defb8f8cedc1b2764a305409b400ab5
Aaron Lee <aaronlee97667@gmail.com>
2023-04-25 11:19:42 -0500

    Updated UI for Home Page

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import { NavLink } from "react-router-dom";
 
 function Home() {
   return (
@@ -7,45 +8,62 @@ function Home() {
       <div className="temp-container">
         <img src={require("../images/splash.png")} alt=""></img>
       </div>
-      <div className="container-fluid">
-        <div className="row row-cols-2 d-flex mt-5 justify-content-center">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+      <div class="container">
+        <div class="row row-cols-2">
+          <div className="col">
+            <div className="card card-containers">
+              <div className="card-body">
+                <h5 className="card-title">Vitality</h5>
+                <p className="card-text">
+                  Vitality is designed to help you figure out your workout plans
+                  based on your desires and interests.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+          <div className="col">
+            <div className="card card-containers">
+              <div className="card-body">
+                <h5 className="card-title">How is Vitality Different?</h5>
+                <p className="card-text">
+                  Vitality filters your selectable exercises based on what you
+                  tell it. Whether it's biceps, triceps, quads, you decide what
+                  exercises you want to see.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+          <div className="col">
+            <div className="card card-containers">
+              <div className="card-body">
+                <h5 className="card-title">User Friendly</h5>
+                <p className="card-text">
+                  A lot of websites are over-complicated and overwhelm you with
+                  all the features on one page. You worry about the plan, let us
+                  worry about getting you there.
+                </p>
+              </div>
             </div>
           </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
+          <div className="col">
+            <div className="card card-containers">
+              <div className="card-body">
+                <h5 className="card-title">Track Your Growth</h5>
+                <p className="card-text">
+                  Keep track of your workouts by the day, and see live numbers
+                  that show you how much progress you've made.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="container text-center">
+        <NavLink to="/signup">
+          <button type="button" className="btn btn-success get-started-button">
+            Let's get started!
+          </button>
+        </NavLink>
       </div>
     </>
   );
