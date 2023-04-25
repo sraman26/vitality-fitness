85e1031ef49ec2b65b73dff6ba2a8d2f86f8941a
Aaron Lee <aaronlee97667@gmail.com>
2023-04-25 18:11:49 -0500

    Home Page UI updated

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -12,45 +12,48 @@ function Home() {
 
   return (
     <>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../images/splash.png")}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../images/sunset_runner.jpg")}
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../images/sunset_runners.jpg")}
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div className="carousel-index">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../images/splash.png")}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../images/sunset_runner.jpg")}
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../images/sunset_runners.jpg")}
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../images/Jumping.png")}
+              alt="Fourth slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../images/sunset-swim.jpg")}
+              alt="Fifth slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <div className="d-flex justify-content-center">
+        <h1 className="home-title">Vitality Fitness</h1>
+      </div>
       <div className="container">
         <div className="row row-cols-2">
           <div className="col">
