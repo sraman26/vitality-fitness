5f5c0b4a74fa7d121d2d02f3a16abebd30e46885
unknown <tbb257@nyu.edu>
2023-04-25 17:13:14 -0400

    Added Carousel code. Coding still works, carousel yet to scroll.

--- ghi/src/components/Home.jsx
+++ ghi/src/components/Home.jsx
@@ -4,12 +4,10 @@ import { NavLink } from "react-router-dom";
 import { useGetAccountQuery } from "../services/workout";
 
 function Home() {
-
   const { data: account, isLoading } = useGetAccountQuery();
   if (isLoading) {
     return;
   }
-
 
   return (
     <>
@@ -68,15 +66,109 @@ function Home() {
       </div>
       {!account && (
         <div className="container text-center">
-        <NavLink to="/signup">
-          <button type="button" className="btn btn-success get-started-button">
-            Let's get started!
-          </button>
-        </NavLink>
+          <NavLink to="/signup">
+            <button
+              type="button"
+              className="btn btn-success get-started-button"
+            >
+              Let's get started!
+            </button>
+          </NavLink>
+        </div>
+      )}
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide"
+        data-bs-ride="false"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={require("../images/splash.png")}
+              className="d-block w-100"
+              alt="..."
+            ></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../images/splash.png")}
+              className="d-block w-100"
+              alt="..."
+            ></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../images/splash.png")}
+              className="d-block w-100"
+              alt="..."
+            ></img>
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-      )
-    }
-
     </>
   );
 }
