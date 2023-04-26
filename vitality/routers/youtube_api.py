31f0fcfde29c2520a32c63acacdd2bf2d718ab79
Michael Miklius <mikliumi@gmail.com>
2023-04-26 11:31:56 -0500

    youtube api working but not yet connected to front end

---
+++ vitality/routers/youtube_api.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, APIRouter
+from auth import authenticator
+import os
+import requests
+from serpapi import GoogleSearch
+
+router = APIRouter()
+youtube_api_key = os.environ["YOUTUBE_VIDEO_API"]
+
+
+def get_youtube_result(params):
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    video_results = results["video_results"][0]["link"]
+    video_results = video_results.split("=")
+    video_results = video_results[1]
+    return video_results
+
+
+@router.get("/api/youtube/")
+def get_youtube_video(search: str):
+    youtube_params = {
+        "engine": "youtube",
+        "search_query": search,
+        "api_key": youtube_api_key,
+    }
+    return get_youtube_result(youtube_params)
