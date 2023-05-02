88f5a85e2e5df5ade4043a6a7429f28ab7c7e9a9
Michael Miklius <mikliumi@gmail.com>
2023-05-02 13:44:06 -0500

    modified cicd again XIII

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -15,15 +15,13 @@ import ErrorPage from "./components/ErrorPage";
 import { useGetAccountQuery } from "./services/workout";
 
 function App() {
-  const domain = /https:\/\/[^/]+/;
-  const basename = process.env.PUBLIC_URL.replace(domain, '');
   const { data: account, isLoading } = useGetAccountQuery();
   if (isLoading) {
     return;
   }
 
   return (
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <Nav />
       <div className="">
         <Routes>
